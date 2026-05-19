f8911e5744644387f445b4edd269a5dac2d0b62d
Abu Saleh Al Amin <142329940+alaminsaleh@users.noreply.github.com>
2026-05-19 16:03:27 +0600

    Update YouDo2.py

--- YouDo2.py
+++ YouDo2.py
@@ -1,10 +1,1 @@
-movieTitle = "4 Idiots"
-releaseYear = 2005
-
-
-print (f"The title {movieTitle} and the release Year was {releaseYear}")
-
-#check the type of the variable
-print("Movie Title", type(movieTitle))
-print("release Year", type(releaseYear))
 
