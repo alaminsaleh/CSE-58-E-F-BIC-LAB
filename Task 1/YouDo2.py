d96c3041c41fb8964b2c21fb70fd941b6ffb9948
Abu Saleh Al Amin <142329940+alaminsaleh@users.noreply.github.com>
2026-05-19 16:02:08 +0600

    Create YouDo2.py

---
+++ Task 1/YouDo2.py
@@ -0,0 +1,9 @@
+movieTitle = "4 Idiots"
+releaseYear = 2005
+
+
+print (f"The title {movieTitle} and the release Year was {releaseYear}")
+
+print("Movie Title", type(movieTitle))
+print("release Year", type(releaseYear))
+
