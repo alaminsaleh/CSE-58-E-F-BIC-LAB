5f7cff507ab6c636d1a1141a28258564cfc89455
Abu Saleh Al Amin <142329940+alaminsaleh@users.noreply.github.com>
2026-06-09 03:11:42 -0700

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,17 @@
-
+def frequent_words(text, k):
+    freq={}
+    for i in range(len(text)-k+1):
+        pattern=text[i:i+k]
+        if pattern in freq:
+            freq[pattern] += 1
+        else:
+            freq[pattern] = 1
+    max_count = max(freq.values())
+    result = []
+    for pattern in freq:
+        if freq[pattern]==max_count:
+            result.append(pattern)
+    return result
+text="ACGTTGCATGTCGCATGATGCATGAGAGCT"
+k = 4
+print(" ".joint(frequent_words(text,)))
